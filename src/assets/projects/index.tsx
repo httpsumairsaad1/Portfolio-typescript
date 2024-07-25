904fe45f0f3ea915738229b6af80e12cf7808431
Umair Saad <121795621+httpsumairsaad1@users.noreply.github.com>
2024-07-25 07:29:13 +0500

    Update index.tsx

--- src/assets/projects/index.tsx
+++ src/assets/projects/index.tsx
@@ -25,35 +25,6 @@ import micro_icon from "./icons/micro_icon.png"
 
 // =============================
 
-import whiteboard_icon from "./icons/whiteboard-icon.png";
-import whiteboard_dark from "./whiteboard-dark.png";
-import whiteboard_light from "./whiteboard-light.png";
-
-import joke_icon from "./icons/joke-icon.png";
-import joke from "./joke.png";
-
-import music_icon from "./icons/music-icon.png";
-import music from "./music.png";
-
-import progress_icon from "./icons/progress-icon.png";
-import progress from "./progress.png";
-
-import quote_icon from "./icons/quote-icon.png";
-import quote from "./quote.png";
-
-import skeleton_icon from "./icons/skeleton-icon.png";
-import skeleton from "./skeleton.png";
-
-import slideshow_icon from "./icons/slideshow-icon.png";
-import slideshow from "./slideshow.png";
-
-import clock_dark from "./clock-dark.png";
-import clock_light from "./clock-light.png";
-import clock_icon from "./icons/clock-icon.png";
-
-import calculator from "./calculator.png";
-import calculator_icon from "./icons/calculator-icon.ico";
-
 export default {
   hamdard_light,
   hamdard_dark,
@@ -78,34 +49,4 @@ export default {
   micro_dark,
   micro,
   micro_icon, 
-  // ===========================
-
-  whiteboard_dark,
-  whiteboard_light,
-  whiteboard_icon,
-
-  joke,
-  joke_icon,
-
-  music,
-  music_icon,
-
-  quote,
-  quote_icon,
-
-  progress,
-  progress_icon,
-
-  skeleton,
-  skeleton_icon,
-
-  slideshow,
-  slideshow_icon,
-
-  calculator,
-  calculator_icon,
-
-  clock_dark,
-  clock_light,
-  clock_icon,
 };
